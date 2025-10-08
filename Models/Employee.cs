e51538bf8fa70fb4ada5e458cba0a122bf62646d
PauliusSILK <726259@student.inholland.nl>
2025-10-08 18:55:48 +0200

    Added Employee Model

---
+++ Models/Employee.cs
@@ -0,0 +1,59 @@
+﻿using System.Net.Sockets;
+using System.Text.Json.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using NoSQL_Project.Models.Enums;
+
+namespace NoSQL_Project.Models
+{
+    public class Employee
+    {
+            [BsonId]
+            [BsonRepresentation(BsonType.ObjectId)]
+            public string Id { get; set; }
+
+            [BsonElement("Name")]
+            public Name Name { get; set; }
+
+            [BsonRepresentation(BsonType.String)]
+            public RoleType Role { get; set; }
+
+            [BsonElement("Email")]
+            public string Email { get; set; }
+
+            [BsonElement("Phone")]
+            public string Phone { get; set; }
+
+            [BsonElement("Location")]
+            public string Location { get; set; }
+
+            [BsonElement("Password")]
+            public string Password { get; set; }
+
+            [BsonElement("contactInfo")]
+            public ContactInfo ContactInfo { get; set; }
+
+        }
+
+        public class Name
+        {
+            [BsonElement("FirstName")]
+            public string FirstName { get; set; }
+
+            [BsonElement("LastName")]
+            public string LastName { get; set; }
+        }
+
+        public class ContactInfo
+        {
+            [BsonElement("Email")]
+            public string Email { get; set; }
+
+            [BsonElement("Phone")]
+            public string Phone { get; set; }
+
+            [BsonElement("Location")]
+            public string Location { get; set; }
+        }
+
+}
---
+++ Models/Enums/RoleType.cs
@@ -0,0 +1,8 @@
+﻿namespace NoSQL_Project.Models.Enums
+{
+    public enum RoleType
+    {
+        Employee,
+        Service_Desk_Employee
+    }
+}
